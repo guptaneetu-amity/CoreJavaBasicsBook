cbbe327b5161bdf6e1f782841d45a793b8983d44
guptaneetu-amity <42129868+guptaneetu-amity@users.noreply.github.com>
2018-08-06 13:45:14 +0530

    Add files via upload

--- TestHello.java
+++ TestHello.java
@@ -1,24 +1,13 @@
 package book;
-//PROGRAM 1.7.2
 
-/* Program to demonstrate command line arguments
-*/
+////PROGRAM 1.7.1
+/* A demo program to demonstrate command line arguments */
 
-public class TestHello { 
-	public static void main (String args[]) 
-	{
-			System.out.println ("Hello!! I am" + args[0]);
+public class TestHello {
+
+	public static void main (String args[]) {
+			System.out.println ("Hello!! I am Fred");
 			System.out.println ("This is my first Java Program");
 	}
 }
-
-////PROGRAM 1.7.1
-//
-//public class TestHello {
-//
-//	public static void main (String args[]) {
-//			System.out.println ("Hello!! I am Fred");
-//			System.out.println ("This is my first Java Program");
-//	}
-//}
 
