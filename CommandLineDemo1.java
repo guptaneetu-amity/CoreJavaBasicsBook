a38fe5f4dbbf06f6ac6c9ad8e603a91dece3cf4f
guptaneetu-amity <42129868+guptaneetu-amity@users.noreply.github.com>
2018-08-06 13:45:34 +0530

    Add files via upload

---
+++ CommandLineDemo1.java
@@ -0,0 +1,13 @@
+package book;
+//PROGRAM 1.7.2
+
+/* Program to demonstrate command line arguments
+*/
+
+public class CommandLineDemo1 { 
+	public static void main (String args[]) 
+	{
+			System.out.println ("Hello!! I am " + args[0]);
+			System.out.println ("This is my first Java Program");
+	}
+}
