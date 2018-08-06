bb6f8c624e9cf547105eb4a56414dc91da356d3c
guptaneetu-amity <42129868+guptaneetu-amity@users.noreply.github.com>
2018-08-06 13:30:11 +0530

    Add files via upload

---
+++ FirstProgram.java
@@ -0,0 +1,15 @@
+// PROGRAM 1.3.1
+
+package book;
+
+/* Program to print a simple string on console
+*/
+
+public class FirstProgram {
+
+	public static void main (String args[]) 
+	{
+		System.out.println ("Hello!! My first program");
+	}
+}
+
